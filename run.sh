b8f6edc0ceda9d93524b31676775489b475705ac
Archie M. Vist <archie.m.vist@gmail.com>
2017-01-25 16:47:25 -0500

    Re-organising code to merge in discord.py branch

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m disco.cli --config config.yaml
---
+++ urist.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from secret import token, prefix
+
+description = '''Urist Dicehammer, providing highly-featured dice and Dwarf Fortress memes since 2016.'''.format(prefix,prefix)
+
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+extensions = ['plugins.admin','plugins.dicehammer']
+
+@bot.event
+async def on_ready():
+   print("Connected to Discord.")
+   print(bot.user.name)
+   print(bot.user.id)
+   print("-- loading extensions --")
+   for extension in extensions:
+      try:
+         bot.load_extension(extension)
+         print("Successfully loaded extension '{}'".format(extension))
+      except Exception as e:
+         print("Error loading extension '{}'".format(extension))
+         print("{}: {}".format(type(e).__name__, e))
+   print("-- beginning log --")
+
+@bot.command()
+async def admin():
+   """Gives administrator contact information."""
+   info = await(bot.application_info())
+   mention = info.owner.mention
+   message = "My administrator is the glorious {}. Fear him, for he is mighty.".format(mention)
+   await(bot.say(message))
+
+if __name__ == '__main__':
+   bot.run(token)
