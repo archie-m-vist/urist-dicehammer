a0154c2cc348e86097280f9dc118c356b6a52561
Archie M. Vist <archie.m.vist@gmail.com>
2017-06-19 19:41:05 -0500

    Fixed some missing/misformatted Touhous.

--- data/randList.py
+++ data/randList.py
@@ -2,11 +2,11 @@ import random
 
 touhousByGame = [
    ["Reimu", "Shingyoku", "YuugenMagan", "Mima", "Elis", "Kikuri", "Sariel", "Konngara"],
-   ["Genji", "Rika", "Meira"],
+   ["Genji", "Rika", "Meira", "Marisa"],
    ["Orange", "Kurumi", "Elly", "Yuuka", "Mugetsu", "Gengetsu"],
    ["Ellen", "Kotohime", "Kana", "Rikako", "Chiyuri", "Yumemi", "Ruukoto"],
    ["Sara", "Luize", "Alice", "Yuki", "Mai", "Yumeko", "Shinki"],
-   ["Rumia", "Daiyousei" "Cirno", "Meiling", "Koakuma", "Patchouli", "Sakuya", "Remilia", "Flandre"],
+   ["Rumia", "Daiyousei", "Cirno", "Meiling", "Koakuma", "Patchouli", "Sakuya", "Remilia", "Flandre"],
    ["Letty", "Chen", "Alice", "Lily White", "Lyrica", "Lunasa", "Merlin", "Youmu", "Yuyuko", "Ran", "Yukari"],
    ["Wriggle", "Mystia", "Keine", "Tewi", "Reisen", "Eirin", "Kaguya", "Mokou"],
    ["Aya", "Medicine", "Komachi", "Eiki"],
@@ -15,7 +15,8 @@ touhousByGame = [
    ["Nazrin", "Kogasa", "Ichirin", "Unzan", "Murasa", "Shou", "Byakuren", "Nue"],
    ["Kyouko", "Yoshika", "Seiga", "Tojiko", "Futo", "Miko", "Mamizou"],
    ["Wakasagihime", "Sekibanki", "Kagerou", "Benben", "Yatsuhashi", "Seija", "Shinmyoumaru", "Raiko"],
-   ["Seiran", "Ringo", "Doremy", "Sagume", "Clownpiece", "Junko", "Hecatia"]
+   ["Seiran", "Ringo", "Doremy", "Sagume", "Clownpiece", "Junko", "Hecatia"],
+   ["Eternity", "Nemuno", "Aunn"]
 ]
 
 spinoffNumbers = {
@@ -48,12 +49,15 @@ touhousPC98 = [i for lst in touhousByGame[0:5] for i in lst]
 touhousPC98Returning = ["Reimu", "Marisa", "Yuuka", "Alice"]
 touhousWindowsOnly = [i for lst in touhousByGame[5:] for i in lst] + [i for lst in spinoffGames for i in lst]
 touhousWindows = touhousPC98Returning + touhousWindowsOnly
+touhousNoPC98 = touhousWindows + [i for lst in otherTouhous for i in lst]
 touhous = touhousPC98 + touhousWindowsOnly + [i for lst in otherTouhous for i in lst]
+touhouModern = touhousWindows + [i for lst in otherTouhous for i in lst]
 
 poketypes = ["Normal", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy", "Steel", "Ghost", "Bug", "Rock", "Ground", "Poison", "Flying", "Fighting"]
 
 lists = {
    "touhou" : touhous,
+   "touhou modern" : touhousNoPC98,
    "poketype" : poketypes
 }
 
