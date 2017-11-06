f1c49b15de7609da1ad35585bdef138e9985b2c0
Archie M. Vist <archie.m.vist@gmail.com>
2017-11-06 04:53:32 +0000

    Updated random lists with Dark Heresy psyker powers and PC-98 only Touhou option.

--- data/randList.py
+++ data/randList.py
@@ -5,7 +5,7 @@ touhousByGame = [
    ["Genji", "Rika", "Meira", "Marisa"],
    ["Orange", "Kurumi", "Elly", "Yuuka", "Mugetsu", "Gengetsu"],
    ["Ellen", "Kotohime", "Kana", "Rikako", "Chiyuri", "Yumemi", "Ruukoto"],
-   ["Sara", "Luize", "Alice", "Yuki", "Mai", "Yumeko", "Shinki"],
+   ["Sara", "Luize", "Alice", "Yuki", "Mai (PC-98)", "Yumeko", "Shinki"],
    ["Rumia", "Daiyousei", "Cirno", "Meiling", "Koakuma", "Patchouli", "Sakuya", "Remilia", "Flandre"],
    ["Letty", "Chen", "Alice", "Lily White", "Lyrica", "Lunasa", "Merlin", "Youmu", "Yuyuko", "Ran", "Yukari"],
    ["Wriggle", "Mystia", "Keine", "Tewi", "Reisen", "Eirin", "Kaguya", "Mokou"],
@@ -16,7 +16,7 @@ touhousByGame = [
    ["Kyouko", "Yoshika", "Seiga", "Tojiko", "Futo", "Miko", "Mamizou"],
    ["Wakasagihime", "Sekibanki", "Kagerou", "Benben", "Yatsuhashi", "Seija", "Shinmyoumaru", "Raiko"],
    ["Seiran", "Ringo", "Doremy", "Sagume", "Clownpiece", "Junko", "Hecatia"],
-   ["Eternity", "Nemuno", "Aunn"]
+   ["Eternity", "Nemuno", "Aunn", "Narumi", "Satono", "Teireida Mai", "Okina"]
 ]
 
 spinoffNumbers = {
@@ -45,20 +45,31 @@ otherTouhous = [
    ["Akyuu"]
 ]
 
+# dark heresy psyker powers
+biomancy = ["Agony","Bio-Lightning","Blood Boil","Cellular Control","Constrict","Drain Vigour","Enhanced Senses","Flesh Like Iron","Hammerhand","Regenerate","Seal Wounds","Shape Flesh","Toxic Siphon"]
+divination = ["Divine Shot","Dowsing","Far Sight","Glimpse","Personal Augery","Precognitive Dodge","Precognitive Strike","Preternatural Awareness","Psychometry","Soul Sight"]
+pyrokinesis = ["Blinding Flash","Burning Fist","Call Flame","Douse Flames","Fire Bolt","Fire Storm","Holocaust","Incinerate","Molten Man","Sculpt Flame","Wall of Fire"]
+telekinesis = ["Catch Projectiles","Fling","Force Barrage","Force Bolt","Precision Telekinesis","Psychic Blade","Psychic Crush","Psychokinetic Storm","Push","Telekinesis","Telekinetic Shield"]
+telepathy = ["Beastmaster","Compel","Dominate","Inspire","Mind Scan","Projection","Psychic Shriek","See Me Not","Seed Mind","Soul Killer","Telepathy","Terrify","Zone of Compulsion"]
+powers = biomancy + divination + pyrokinesis + telepathy + telekinesis
+
+# touhous
 touhousPC98 = [i for lst in touhousByGame[0:5] for i in lst]
 touhousPC98Returning = ["Reimu", "Marisa", "Yuuka", "Alice"]
 touhousWindowsOnly = [i for lst in touhousByGame[5:] for i in lst] + [i for lst in spinoffGames for i in lst]
 touhousWindows = touhousPC98Returning + touhousWindowsOnly
 touhousNoPC98 = touhousWindows + [i for lst in otherTouhous for i in lst]
 touhous = touhousPC98 + touhousWindowsOnly + [i for lst in otherTouhous for i in lst]
-touhouModern = touhousWindows + [i for lst in otherTouhous for i in lst]
 
+# pokemon types
 poketypes = ["Normal", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy", "Steel", "Ghost", "Bug", "Rock", "Ground", "Poison", "Flying", "Fighting"]
 
 lists = {
    "touhou" : touhous,
    "touhou modern" : touhousNoPC98,
-   "poketype" : poketypes
+   "touhou pc-98" : touhousPC98,
+   "poketype" : poketypes,
+   "power" : powers
 }
 
 def randomFromList (lst):
